2005edd74969fe0d3469a0990df6f8462ed814ab
Eben <maradebenezer@gmail.com>
2023-04-21 16:51:58 +0100

    removed optimization

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -8,36 +8,26 @@
 void bubble_sort(int *array, size_t size)
 {
 	size_t i, j;
-	bool swapped;
+	int temp;
+	/* bool swapped; */
 
 	for (i = 0; i < size - 1; i++)
 	{
-		swapped = false;
+		/* swapped = false; */
 		for (j = 0; j < size - i - 1; j++)
 		{
 			if (array[j] > array[j + 1])
 			{
-				swap(&array[j], &array[j + 1]);
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
 				print_array(array, size);
-				swapped = true;
+				/* swapped = true; */
 			}
 		}
-		if (swapped == false)
-			break;
+		/*
+		 * if (swapped == false)
+		 *	break;
+		 */
 	}
-}
-
-/**
- * swap - swap two numbers in positions in an array
- * @a: first number
- * @b: second number
- * Return: no return
- */
-void swap(int *a, int *b)
-{
-	int temp;
-
-	temp = *a;
-	*a = *b;
-	*b = temp;
 }
