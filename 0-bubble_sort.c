6343a280ce27e28c0a5b05734a32f0192f1e3f41
Eben <maradebenezer@gmail.com>
2023-04-21 16:54:01 +0100

    added check for empty array:

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -9,11 +9,13 @@ void bubble_sort(int *array, size_t size)
 {
 	size_t i, j;
 	int temp;
-	/* bool swapped; */
+	bool swapped;
 
+	if (array == NULL)
+		return;
 	for (i = 0; i < size - 1; i++)
 	{
-		/* swapped = false; */
+		swapped = false;
 		for (j = 0; j < size - i - 1; j++)
 		{
 			if (array[j] > array[j + 1])
@@ -22,12 +24,10 @@ void bubble_sort(int *array, size_t size)
 				array[j] = array[j + 1];
 				array[j + 1] = temp;
 				print_array(array, size);
-				/* swapped = true; */
+				swapped = true;
 			}
 		}
-		/*
-		 * if (swapped == false)
-		 *	break;
-		 */
+		if (swapped == false)
+			break;
 	}
 }
